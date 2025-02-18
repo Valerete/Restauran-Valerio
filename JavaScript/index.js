f4b151d3725cc820956c3f66eb16c6248fcf5fa9
Valery <valeryhenriquez13@gmail.com>
2025-02-18 08:56:21 -0600

    Mi quinto commit

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -2,32 +2,4 @@ let navbar = document.querySelector('.navbar');
 
 document.querySelector('#Menú-btn').onclick = () =>{
     navbar.classList.toggle('active');
-    searchForm.classList.remove('active');
-    cartItem.classList.remove('active');
-
-
-}
-let SearchForm= document.querySelector('.search-form');
-document.querySelector('#search-btn').onclick = () =>{
-    SearchForm.classList.toggle('active');
-    navbar.classList.remove('active');
-    cartItem.classList.remove('active');
-
-
-}
-
-let cartItem = document.querySelector('.cart-items-container');
-document.querySelector('#cart-btn').onclick = () =>{
-    cartItem.classList.toggle('active'); 
-    navbar.classList.remove('active');
-    SearchFormearchform.classList.remove('active');
-   
-
-
-}
-window.onscroll = () =>{
-    navbar.classList.remove('active');
-    SearchForm.classList.remove('active');
-    cartItem.classList.remove('active');
-
 }
